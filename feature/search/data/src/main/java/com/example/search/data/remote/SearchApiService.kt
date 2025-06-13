fe1ed4c9f12dd379a55cc0db8f6fe8d988b7c159
Anu <anuavr123@gmail.com>
2025-06-13 17:05:14 +0530

    added recipedetails api endpoint and model

--- feature/search/data/src/main/java/com/example/search/data/remote/SearchApiService.kt
+++ feature/search/data/src/main/java/com/example/search/data/remote/SearchApiService.kt
@@ -1,4 +1,18 @@
 package com.example.search.data.remote
 
+import com.example.search.data.model.RecipeDetailResponse
+import com.example.search.data.model.RecipeResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+//https://www.themealdb.com/api/json/v1/1/search.php?s=potato
 interface SearchApiService {
+    @GET("api/json/v1/1/search.php")
+    suspend fun getRecipes(@Query("s") s: String): Response<RecipeResponse>
+
+    @GET("api/json/v1/1/lookup.php")
+    suspend fun getRecipeDetails(@Query("i") s: String): Response<RecipeDetailResponse>
+
+
 }
