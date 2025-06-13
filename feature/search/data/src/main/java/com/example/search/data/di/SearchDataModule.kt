e2634a8aa4e353f4677c22c7a007ae900bea4a0d
Anu <anuavr123@gmail.com>
2025-06-13 18:11:28 +0530

    created seperate modeule for retrofit and search

--- feature/search/data/src/main/java/com/example/search/data/di/SearchDataModule.kt
+++ feature/search/data/src/main/java/com/example/search/data/di/SearchDataModule.kt
@@ -4,25 +4,12 @@ import com.example.search.data.remote.SearchApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ActivityComponent
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
-const val BASE_URL = "https://www.themealdb.com/"
-
-@InstallIn(SingletonComponent::class)
+@InstallIn(ActivityComponent::class)
 @Module
 object SearchDataModule {
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder().baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
     @Provides
     fun provideSearchAPIService(retrofit: Retrofit): SearchApiService {
         return retrofit.create(SearchApiService::class.java)
---
+++ feature/search/data/src/main/java/com/example/search/data/di/SearchRetrofitDataModule.kt
@@ -0,0 +1,27 @@
+package com.example.search.data.di
+
+import com.example.search.data.remote.SearchApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+const val BASE_URL = "https://www.themealdb.com/"
+
+@InstallIn(SingletonComponent::class)
+@Module
+object SearchRetrofitDataModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+
+}
