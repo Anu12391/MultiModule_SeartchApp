9fa4557ff219aff15d7f0dc4277ded58bfe25cc2
Anu <anuavr123@gmail.com>
2025-06-14 19:29:35 +0530

    created mapper file and mapped RecipeDTO to Recipes

--- feature/search/data/src/main/java/com/example/search/data/repository/SearchRepoImpl.kt
+++ feature/search/data/src/main/java/com/example/search/data/repository/SearchRepoImpl.kt
@@ -1,12 +1,13 @@
 package com.example.search.data.repository
 
+import com.example.search.data.remote.SearchApiService
 import com.example.search.domain.model.Recipes
 import com.example.search.domain.model.RecipesDetails
 import com.example.search.domain.repository.SearchRepository
 
-class SearchRepoImpl: SearchRepository {
-    override suspend fun getRecipes(): List<Recipes> {
-        TODO("Not yet implemented")
+class SearchRepoImpl(val searchApiService: SearchApiService) : SearchRepository {
+    override suspend fun getRecipes(s: String): List<Recipes> {
+        searchApiService.getRecipes(s)
     }
 
     override suspend fun getRecipeDetails(id: String): List<RecipesDetails> {
--- feature/search/domain/src/main/java/com/example/search/domain/repository/SearchRepository.kt
+++ feature/search/domain/src/main/java/com/example/search/domain/repository/SearchRepository.kt
@@ -4,6 +4,6 @@ import com.example.search.domain.model.Recipes
 import com.example.search.domain.model.RecipesDetails
 
 interface SearchRepository {
-    suspend fun getRecipes(): List<Recipes>
+    suspend fun getRecipes(s:String): List<Recipes>
     suspend fun getRecipeDetails(id:String): List<RecipesDetails>
 }
