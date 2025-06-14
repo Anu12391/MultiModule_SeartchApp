749908ee4cd1d69ff9ce10fdeb39d02940e9660d
Anu <anuavr123@gmail.com>
2025-06-14 19:46:36 +0530

    added code to get recipe and recipe details in serach repository-----incomplete

--- feature/search/data/src/main/java/com/example/search/data/repository/SearchRepoImpl.kt
+++ feature/search/data/src/main/java/com/example/search/data/repository/SearchRepoImpl.kt
@@ -1,16 +1,41 @@
 package com.example.search.data.repository
 
+import com.example.search.data.mappers.toDomain
 import com.example.search.data.remote.SearchApiService
 import com.example.search.domain.model.Recipes
 import com.example.search.domain.model.RecipesDetails
 import com.example.search.domain.repository.SearchRepository
 
 class SearchRepoImpl(val searchApiService: SearchApiService) : SearchRepository {
-    override suspend fun getRecipes(s: String): List<Recipes> {
-        searchApiService.getRecipes(s)
+    override suspend fun getRecipes(s: String): Result<List<Recipes>> {
+        val response = searchApiService.getRecipes(s)
+        return if (response.isSuccessful) {
+            response.body()?.meals?.let {
+                Result.success(it.toDomain())
+            }?:run {
+                Result.failure(Exception("error occured"))
+            }
+
+        } else {
+            Result.failure(Exception("error occured"))
+        }
+
     }
 
-    override suspend fun getRecipeDetails(id: String): List<RecipesDetails> {
-        TODO("Not yet implemented")
+    override suspend fun getRecipeDetails(id: String): Result<List<RecipesDetails>> {
+        val response=searchApiService.getRecipeDetails(id)
+       return if(response.isSuccessful){
+            response.body()?.meals?.let{
+                if(it.isNotEmpty()){
+                    it.toDomain()
+                }else{
+                    Result.failure(Exception("error occured"))
+
+                }
+                it.toDomain()
+            }?:run {   Result.failure(Exception("error occured")) }
+        }else{
+           Result.failure(Exception("error occured"))
+       }
     }
 }
--- feature/search/domain/src/main/java/com/example/search/domain/repository/SearchRepository.kt
+++ feature/search/domain/src/main/java/com/example/search/domain/repository/SearchRepository.kt
@@ -4,6 +4,6 @@ import com.example.search.domain.model.Recipes
 import com.example.search.domain.model.RecipesDetails
 
 interface SearchRepository {
-    suspend fun getRecipes(s:String): List<Recipes>
-    suspend fun getRecipeDetails(id:String): List<RecipesDetails>
+    suspend fun getRecipes(s:String): Result<List<Recipes>>
+    suspend fun getRecipeDetails(id:String): Result<List<RecipesDetails>>
 }
