7d2b9457cdfd78241a9a077a34f1bf54978810a7
Anu <anuavr123@gmail.com>
2025-06-19 18:45:21 +0530

    created use case for fetching recipes

--- feature/search/data/src/main/java/com/example/search/data/use_cases/GetAllRecipesUseCases.kt
+++ feature/search/data/src/main/java/com/example/search/data/use_cases/GetAllRecipesUseCases.kt
@@ -1,12 +1,27 @@
 package com.example.search.data.use_cases
 
 import com.example.common.utils.NetworkResult
-import com.example.search.data.repository.SearchRepoImpl
+import com.example.search.domain.model.Recipes
+
 import com.example.search.domain.repository.SearchRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetAllRecipesUseCases constructor(@Inject val searchRepository: SearchRepository) {
-operator fun invoke(q:String)= flow<NetworkResult> {  }
+    operator fun invoke(q: String) = flow<NetworkResult<List<Recipes>>> {
+        emit(NetworkResult.Loading())
+        val response = searchRepository.getRecipes(q)
+        if (response.isSuccess) {
+            emit(NetworkResult.Success(response.getOrThrow()))
+
+        } else {
+            emit(NetworkResult.Error(message = response.exceptionOrNull()?.localizedMessage))
+        }
+    }.catch {
+        emit(NetworkResult.Error(message = it.localizedMessage ?: "Unexpected error occurred"))
+    }.flowOn(Dispatchers.IO)
 
 }
