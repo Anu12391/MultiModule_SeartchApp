fd5db32c2d69db569458ddcf19fb481f6b9738f0
Anu <anuavr123@gmail.com>
2025-06-19 17:47:34 +0530

    created UiText class for strings

--- common/src/main/java/com/example/common/utils/UiText.kt
+++ common/src/main/java/com/example/common/utils/UiText.kt
@@ -1,4 +1,25 @@
 package com.example.common.utils
 
+import android.content.Context
+import androidx.annotation.StringRes
+
 sealed class UiText {
+    data class RemoteString(val message:String):UiText()
+    class LocalString(@StringRes val res:Int,vararg val args:Any):UiText()
+
+    data object Idle:UiText()
+
+    fun getString(context:Context):String{
+        return when(this){
+            is RemoteString->{
+                message
+
+        }
+
+            Idle -> {
+                ""
+            }
+            is LocalString -> {context.getString(res,*args)}
+        }
+    }
 }
