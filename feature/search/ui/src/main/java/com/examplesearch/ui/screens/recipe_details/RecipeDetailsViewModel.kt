3b9e5df687589f1020736f91d2a2712b85ce4bae
Anu <anuavr123@gmail.com>
2025-07-04 18:05:19 +0530

    called getrecipedetails api and added condition states

--- feature/search/ui/src/main/java/com/examplesearch/ui/screens/recipe_details/RecipeDetailsViewModel.kt
+++ feature/search/ui/src/main/java/com/examplesearch/ui/screens/recipe_details/RecipeDetailsViewModel.kt
@@ -1,24 +1,34 @@
 package com.examplesearch.ui.screens.recipe_details
 
 import androidx.lifecycle.ViewModel
+import com.example.common.utils.NetworkResult
 import com.example.common.utils.UiText
 import com.example.search.data.use_cases.GetRecipeDetailsUseCases
 import com.example.search.domain.model.RecipesDetails
-import com.examplesearch.ui.screens.recipe_list.RecipeList
+
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipeDetailsViewModel @Inject constructor(private val getRecipeDetailsUseCases: GetRecipeDetailsUseCases) :
     ViewModel() {
 
-    private val _uiState = MutableStateFlow(RecipeList.UiState())
-    val uiState: StateFlow<RecipeList.UiState> get() = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(RecipeDetails.UiState())
+    val uiState: StateFlow<RecipeDetails.UiState> get() = _uiState.asStateFlow()
 
+fun recipeDetails(id:String)=getRecipeDetailsUseCases.invoke(id).onEach {
+    result->
+    when(result){
+        is NetworkResult.Error -> TODO()
+        is NetworkResult.Loading -> TODO()
+        is NetworkResult.Success -> TODO()
+    }
 
+}
 
     object RecipeDetails {
         data class UiState(
