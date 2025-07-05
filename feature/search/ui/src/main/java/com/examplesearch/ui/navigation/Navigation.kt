5579b90857890f3c2f15aedb54b18af04d1cd6b8
Anu <anuavr123@gmail.com>
2025-07-05 13:57:27 +0530

    composable not working-incomplete

--- feature/search/ui/src/main/java/com/examplesearch/ui/navigation/Navigation.kt
+++ feature/search/ui/src/main/java/com/examplesearch/ui/navigation/Navigation.kt
@@ -1,15 +1,25 @@
 package com.examplesearch.ui.navigation
 
+import androidx.navigation.navigation
 import com.example.common.navigation.FeatureApi
+import com.example.common.navigation.NavigationRoutes
+import com.example.common.navigation.NavigationSubGraphRoutes
 
-interface SearchFeatureApi: FeatureApi
+interface SearchFeatureApi : FeatureApi
 
-class SearchFeatureApiImpl:SearchFeatureApi{
+class SearchFeatureApiImpl : SearchFeatureApi {
     override fun registerGraph(
         navGraphBuilder: androidx.navigation.NavGraphBuilder,
         navHostController: androidx.navigation.NavHostController
     ) {
+        navGraphBuilder.navigation(
+            route = NavigationSubGraphRoutes.Search.routes,
+            startDestination = NavigationRoutes.RecipeList.routes
+        ) {
+           // composable(route = NavigationRoutes.RecipeList.routes) {
 
+            }
+        }
     }
 
 }
